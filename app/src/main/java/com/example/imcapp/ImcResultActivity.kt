43f2b0747efed1ba52cbe6b9dd59d44719561f6e
carloport <carleslop05@gmail.com>
2024-11-06 13:55:35 +0100

    v1.6.1

--- app/src/main/java/com/example/imcapp/ImcResultActivity.kt
+++ app/src/main/java/com/example/imcapp/ImcResultActivity.kt
@@ -1,12 +1,20 @@
 package com.example.imcapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ImcResultActivity : AppCompatActivity() {
+    private lateinit var tv_category: TextView
+    private lateinit var tvNumber: TextView
+    private lateinit var tvDescription: TextView
+    private lateinit var btnCalcular: AppCompatButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,21 @@ class ImcResultActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        initComponents()
+        initListeners()
+        initUI()
+    }
+
+    private fun initUI() {
+        TODO("Not yet implemented")
+    }
+
+    private fun initListeners() {
+        TODO("Not yet implemented")
+    }
+
+    private fun initComponents() {
+        TODO("Not yet implemented")
     }
 }
